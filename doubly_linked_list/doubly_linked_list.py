c70576d751b587bfea593c3eb245fa96b901ac7a
Your Name <you@example.com>
2020-07-25 15:06:18 -0400

    doubly-linked-list/singly-stack

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -34,16 +34,46 @@ class DoublyLinkedList:
     current head's next node the new head of the List.
     Returns the value of the removed Node.
     """
+    
     def remove_from_head(self):
-        pass
+        if self.length == 0:
+            # return undefined
+            old_head = self.head
+
+        elif self.length ==1:
+            self.head is None
+            self.tail is None
+
+        else:
+            self.head = old_head.next
+            self.head.prev is None
+            old_head.next is None
             
+            self.length -=1
+            # return old_head
+
+   
+    
+
     """
     Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        cur_node = ListNode(value)
+
+        if self.length is None:
+            self.head = cur_node
+            self.tail = cur_node
+        else:
+            self.tail.next = cur_node
+            cur_node.prev = self.tail
+            self.tail = cur_node
+
+            self.length += 1
+            # return cur_node
+
             
     """
     Removes the List's current tail node, making the 
@@ -51,7 +81,17 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        if self.head == 0:
+            return None
+        
+        else:
+            old_tail = self.tail
+            self.tail = old_tail.prev
+            self.tail.next = None	
+            old_tail.prev = None
+        
+            self.length -= 1
+            # return old_tail
             
     """
     Removes the input node from its current spot in the 
@@ -72,11 +112,31 @@ class DoublyLinkedList:
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
+        cur_node = self.head
+        while cur_node != node:
+            cur_node = cur_node.next
+
+            cur_node.prev.next = cur_node.next
+            cur_node.next.prev = cur_node.prev
+
+    # a way to do it without a loop because we are passing in a node not a value
+    #  def delete(self, node: ListNode):
+    #     node.prev.next = node.next 
+    # -- look at what comes after node and set its prev pointer = 
+    #     to original node's prev pointer
+    #     node.next.prev = node.prev
+
+
 
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
     def get_max(self):
-        pass
+        cur_node = self.head
+        cur_max = self.head.value
+        while cur_node is not None:
+            if cur_node.value > cur_max:
+                cur_max = cur_node.value
+            cur_node = cur_node.next
+        return cur_max 
--- stack/stack.py
+++ stack/stack.py
@@ -10,10 +10,29 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+#         self.items = []
+        
+#     def isEmpty(self):
+#         return self.items == []
+    
+#     def push(self,item):
+#         self.items.append(item)
+        
+#     def pop(self):
+#         return self.items.pop()
+    
+#     def peek(self):
+#         return self.items[len(self.items)-1]
+    
+#     def size(self):
+#         return len(self.items)
+    
 class Stack:
-    def __init__(self):
+    def __init__(self, LinkedList):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
         pass
